24977e9e95ded88e71474ee5272d4030da9662c2
naybyal <officiallynabiel@gmail.com>
2023-09-28 12:00:08 +0530

    Change

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
-// import logo from './logo.svg';
-import './App.css';
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import { props } from react;
-const name = 'React';
-// const isNameDisplayed = false;
-const Person = () => {
-  return (
-    <>
-      <h1>Name : {this.props.name}</h1>
-      <h2>LName : {this.props.lname}</h2>
-      <h2>Age : 22</h2>
-    </>
-  )
-}
+import React from 'react';
+
 const App = () => {
-  return (
-    <div className="App">
-      <h1>Hello, { name }!</h1>
-      <Person name='Rosetta' lname='Stoned' />
-    </div>
-  );
+    return (
+        <h1>App</h1>
+    );
 }
 
 export default App;
